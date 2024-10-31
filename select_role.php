59e6cc98e9cea5babf48fec6a840a1c9c4994b14
Caleb Robinson <cmrd2d@umsystem.edu>
2024-10-31 15:36:09 -0500

    default html files

---
+++ select_role.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head lang = "en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Wage Wizards</title>
+</head>
+<body>
+    <h1 class="center">Automatic Salaray by Wage Wizards</h1>
+    <p>company name</p>
+    <div class="select_role">
+        <div>
+            <form class="center" class="role" action="/assoc_home.php" method="POST">
+                <button type="submit" class="item">Assocate</button>
+            </form>
+        </div>
+        <div>
+            <form class="center" class="role" action="/assoc_home.php" method="POST">
+                <button type="submit" class="item">Assocate</button>
+            </form>
+        </div>
+        <div>
+            <form class="center" class="role" action="/assoc_home.php" method="POST">
+                <button type="submit" class="item">Assocate</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html> 
