b33f5dbf5c1519549ab57a7a6200c1db95e53a97
cmrd2d <cmrd2d@umsystem.edu>
2024-11-12 15:44:00 -0600

    store ssn globly

--- front_end/login/login.php
+++ front_end/login/login.php
@@ -20,7 +20,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $password = $_POST['password'];
 
     //do sql quary to see if the username and password are in the db
-    $sql = "SELECT login.username, login.password FROM login JOIN has ON login.username = has.username JOIN employs ON has.E_Ssn = employs.E_Ssn WHERE login.username = '$username' AND employs.C_name = '".$_SESSION['company_name']."'";
+    $sql = "SELECT login.username AS username, login.password AS password, employs.E_Ssn AS ssn FROM login JOIN has ON login.username = has.username JOIN employs ON has.E_Ssn = employs.E_Ssn WHERE login.username = '$username' AND employs.C_name = '".$_SESSION['company_name']."'";
     $result = $conn->query($sql);
 
     //there is username and passwrod combo in the db
@@ -29,6 +29,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         if($row['password'] == $password)
         {
             $_SESSION['username'] = $username;
+            $_SESSION['ssn'] = $row['ssn'];
             header("Location: ../select_role/select_role.html");
             exit();
         }
