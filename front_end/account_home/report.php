5f9dbfafb6ab3ec14297a4976e2231a634c4798a
abrahamyirga <125903442+abrahamyirga@users.noreply.github.com>
2024-11-21 14:08:58 -0600

    tables

---
+++ front_end/account_home/report.php
@@ -0,0 +1,94 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wage Wizards</title>
+</head>
+<body>
+<button onclick="window.history.back()">Back</button>
+<h1>Financial Report</h1>
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "hr_database"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$C_name = $_SESSION['company_name'];
+$sql = "SELECT employs.E_Ssn AS ssn,
+    hr.F_name AS fname,
+    hr.L_name AS lname,
+    pay_details.salary AS salary,
+    pay_details.bonus AS bonus,
+    pay_details.benefits AS benefits
+    FROM employs
+    JOIN gets ON employs.E_Ssn = gets.E_Ssn
+    JOIN pay_details ON gets.pay_id = pay_details.pay_id
+    JOIN hr ON employs.E_Ssn = hr.hr_Ssn
+    WHERE employs.C_name = '$C_name'
+    UNION
+    SELECT employs.E_Ssn AS ssn,
+    accountant.F_name AS fname,
+    accountant.L_name AS lname,
+    pay_details.salary AS salary,
+    pay_details.bonus AS bonus,
+    pay_details.benefits AS benefits
+    FROM employs
+    JOIN gets ON employs.E_Ssn = gets.E_Ssn
+    JOIN pay_details ON gets.pay_id = pay_details.pay_id
+    JOIN accountant ON employs.E_Ssn = accountant.acc_Ssn
+    WHERE employs.C_name = '$C_name'
+    UNION
+    SELECT employs.E_Ssn AS ssn,
+    associate.F_name AS fname,
+    associate.L_name AS lname,
+    pay_details.salary AS salary,
+    pay_details.bonus AS bonus,
+    pay_details.benefits AS benefits
+    FROM employs
+    JOIN gets ON employs.E_Ssn = gets.E_Ssn
+    JOIN pay_details ON gets.pay_id = pay_details.pay_id
+    JOIN associate ON employs.E_Ssn = associate.acc_Ssn
+    WHERE employs.C_name = '$C_name'
+    ";
+
+$result = $conn->query($sql);
+if ($result === FALSE) {
+    // If there was an error, output the SQL query and the error message
+    echo "Error with query: " . $sql . "<br><br><br>";
+    echo "MySQL error: " . $conn->error;
+}
+
+$total_salary = 0;
+$total_bonus = 0;
+$total_benefits = 0;
+if($result->num_rows > 0){
+    echo "<table border='1'><tr><th>SSN</th><th>Employee</th><th>Salary</th><th>Bonus</th><th>Benefits</th><th>Total</th></tr>";
+    while($row = $result->fetch_assoc())
+    {
+        $total = $row['salary'] + $row['bonus'] + $row['benefits'];
+        echo "<tr><td>" . $row['ssn'] . "</td><td>" . $row['fname'] . " " . $row['lname'] . "</td><td>" . $row['salary'] . "</td><td>" . $row['bonus'] . "</td><td>" . $row['benefits'] . "</td><td>$total</td></tr>";
+        echo "<br>";
+        $total_salary = $total_salary + $row['salary'];
+        $total_bonus = $total_bonus + $row['bonus'];
+        $total_benefits = $total_benefits + $row['benefits'];
+    }
+    $total = $total_salary + $total_bonus + $total_benefits;
+    echo "<tr><td></td><td></td><td>$total_salary</td><td>$total_bonus</td><td>$total_benefits</td><td>$total</td></tr>";
+}
+else{
+    echo "Have you company accountant enter the employees pay infomation.";
+}
+
+$conn->close();
+?>
+</body>
+</html>
---
+++ front_end/lookup/lookup.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "hr_database"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the employees' information
+$sql = "SELECT 'hr' AS role, hr.hr_Ssn AS ssn, hr.F_name AS fname, hr.L_name AS lname, hr.email AS email, hr.phone_no AS phone
+        FROM employs 
+        JOIN hr ON employs.E_Ssn = hr.hr_Ssn WHERE employs.C_name = '".$_SESSION['company_name']."'
+        UNION
+        SELECT 'accountant' AS role, accountant.acc_Ssn AS ssn, accountant.F_name AS fname, accountant.L_name AS lname, accountant.email AS email, accountant.phone_no AS phone
+        FROM employs 
+        JOIN accountant ON employs.E_Ssn = accountant.acc_Ssn WHERE employs.C_name = '".$_SESSION['company_name']."'
+        UNION
+        SELECT 'associate' AS role, associate.acc_Ssn AS ssn, associate.F_name AS fname, associate.L_name AS lname, associate.email AS email, associate.phone_no AS phone
+        FROM employs 
+        JOIN associate ON employs.E_Ssn = associate.acc_Ssn WHERE employs.C_name = '".$_SESSION['company_name']."'
+        ORDER BY fname, lname";
+
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wage Wizards</title>
+</head>
+<body>
+
+    <h1>List of Employees</h1>
+    <button onclick="window.history.back()">Back</button>
+
+    <?php
+    // Check if the query returns any results
+    if ($result->num_rows > 0) {
+        // Start the HTML table
+        echo "<table border='1'><tr><th>SSN</th><th>Role</th><th>Name</th><th>Email</th><th>Phone Number</th></tr>";
+
+        // Output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["ssn"] . "</td><td>" . $row["role"] . "</td><td>" . $row["fname"] . " " . $row["lname"] . "</td><td>" . $row["email"] . "</td><td>" . $row["phone"] . "</td></tr>";
+        }
+
+        // End the table
+        echo "</table>";
+    } else {
+        echo "<p>No users found.</p>";
+    }
+
+    // Close the database connection
+    $conn->close();
+    ?>
+
+</body>
+</html>
